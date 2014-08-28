60f5e6cb0619f239838e6402aba4cbdf3b635963
simeonwong <simeon.wong@firmstudio.com>
2014-08-28 13:42:13 +0800

    update list of chairman and images

--- en/list-of-chairmen.php
+++ en/list-of-chairmen.php
@@ -15,7 +15,7 @@
         <img src="../images/about-us/about-us-banner-m.jpg" class="mob tablet"/>
     </div>
     
-    <div class="inner-page">
+    <div class="inner-page chairman-list">
     	<div class="holder">
         	<div class="breadcrumb">
             	<span class="pass">Home</span>
@@ -35,30 +35,30 @@
         <div class="this-year chairman-holder">
         	<div class="holder">
             	<h3>2013 - 2015
-                <span>The 22th General Committee</span></h3>
+                <span>The 13th General Committee</span></h3>
                 
                 <div class="chairman-item">
-                	<img src="" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
+                	<img src="../images/about-us/list-of-chairman/2013-2015/Adam-Lau.png" />
+                    <p>Dr Adam Lau
+                    <span>Chairman</span>
                     </p>
-                </div>
-                <div class="chairman-item">
+                </div><!--
+                --><div class="chairman-item">
                 	<img src="../images/about-us/list-of-chairman/2013-2015/Benny-Do.png" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
+                    <p>Mr Benny Do
+                    <span>Associate Chairman</span>
                     </p>
-                </div>
-                <div class="chairman-item">
+                </div><!--
+                --><div class="chairman-item">
                 	<img src="../images/about-us/list-of-chairman/2013-2015/Bronia-Yip.png" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
+                    <p>Ms Bronia Yip
+                    <span>Associate Chairman</span>
                     </p>
-                </div>
-                <div class="chairman-item">
+                </div><!--
+                --><div class="chairman-item">
                 	<img src="../images/about-us/list-of-chairman/2013-2015/Peter-Lam.png" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
+                    <p>Mr Peter Lam
+                    <span>Associate Chairman</span>
                     </p>
                 </div>
             </div>
@@ -66,59 +66,340 @@
         
         <div class="odd-line chairman-holder">
         	<div class="holder">
-            	<h3>2013 - 2015
-                <span>The 22th General Committee</span></h3>
+            	<h3>2011 - 2013
+                <span>The 12th General Committee</span></h3>
                 
                 <div class="chairman-item">
-                	<img src="../images/about-us/List of Chairmen/1988-1991/Frank Tang (purple).jpg" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
+                	<img src="../images/about-us/list-of-chairman/2011-2013/Ricky_Lam.jpg" />
+                    <p>Mr. Ricky Lam
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2011-2013/Adam_Lau.jpg" />
+                    <p>Mr. Adam Lau
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2011-2013/Benny_Do.jpg" />
+                    <p>Mr. Benny Do
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2011-2013/Bronia_Yip.jpg" />
+                    <p>Ms. Bronia Yip
+                    <span>Associate Chairman & Director of Exhibtion</span>
                     </p>
                 </div>
-                <div class="chairman-item">
-                	<img src="../images/about-us/List of Chairmen/1988-1991/Frank Tang (purple).jpg" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
-                    </p>
-                </div>
-                <div class="chairman-item">
-                	<img src="../images/about-us/List of Chairmen/1988-1991/Frank Tang (purple).jpg" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
-                    </p>
-                </div>
-                
             </div>
         </div>
         
         <div class="even-line chairman-holder">
         	<div class="holder">
-            	<h3>2013 - 2015
-                <span>The 22th General Committee</span></h3>
+            	<h3>2009 - 2011
+                <span>The 11th General Committee</span></h3>
                 
                 <div class="chairman-item">
-                	<img src="../images/about-us/List of Chairmen/1988-1991/Frank Tang (purple).jpg" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
+                	<img src="../images/about-us/list-of-chairman/2009-2011/Sunny_Chan.jpg" />
+                    <p>Mr. Sunny Chan
+                    <span>Chairman</span>
                     </p>
-                </div>
-                <div class="chairman-item">
-                	<img src="../images/about-us/List of Chairmen/1988-1991/Frank Tang (purple).jpg" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2009-2011/Benny_Do.jpg" />
+                    <p>Mr. Benny_Do
+                    <span>Associate Chairman</span>
                     </p>
-                </div>
-                <div class="chairman-item">
-                	<img src="../images/about-us/List of Chairmen/1988-1991/Frank Tang (purple).jpg" />
-                    <p>Frank Tang
-                    <span>Founding Chairman</span>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2009-2011/Ricky_Lam.jpg" />
+                    <p>Mr. Ricky_Lam
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2009-2011/Adam_Lau.jpg" />
+                    <p>Dr. Adam_Lau
+                    <span>Associate Chairman</span>
                     </p>
                 </div>
                 
             </div>
         </div>
         
+        <div class="odd-line chairman-holder">
+        	<div class="holder">
+            	<h3>2007 - 2009
+                <span>The 10th General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2007-2009/Addy_Wong.jpg" />
+                    <p>Mr. Addy Wong
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2007-2009/Sunny_Chan.jpg" />
+                    <p>Mr. Sunny Chan
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2007-2009/Karl_Shin.jpg" />
+                    <p>Mr. Karl Shin
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2007-2009/Ricky_Lam.jpg" />
+                    <p>Ms. Ricky Lam
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
         
+        <div class="even-line chairman-holder">
+        	<div class="holder">
+            	<h3>2005 - 2007
+                <span>The 9th General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2005-2007/Edward_Cheung.jpg" />
+                    <p>Mr. Edward_Cheung
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2005-2007/Addy_Wong.jpg" />
+                    <p>Mr. Addy_Wong
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2005-2007/Louis_Lo.jpg" />
+                    <p>Mr. Louis_Lo
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2005-2007/Victor_Kei.jpg" />
+                    <p>Dr. Victor_Kei
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="odd-line chairman-holder">
+        	<div class="holder">
+            	<h3>2003 - 2005
+                <span>The 8th General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2003-2005/Aaron_Shum.jpg" />
+                    <p>Mr. Aaron Shum
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2003-2005/Edward_Cheung.jpg" />
+                    <p>Mr. Edward Cheung
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2003-2005/Louis_Lo.jpg" />
+                    <p>Mr. Louis Lo
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="even-line chairman-holder">
+        	<div class="holder">
+            	<h3>2001 - 2003
+                <span>The 7th General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2001-2003/Patrick_Luk.jpg" />
+                    <p>Mr. Patrick Luk
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2001-2003/Louis_Lo.jpg" />
+                    <p>Mr. Louis Lo
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/2001-2003/Victor_Kei.jpg" />
+                    <p>Mr. Victor Kei
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="odd-line chairman-holder">
+        	<div class="holder">
+            	<h3>1999 - 2001
+                <span>The 6th General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1999-2001/Dennis_Ng.jpg" />
+                    <p>Mr. Dennis Ng
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1999-2001/Alex_Lee.jpg" />
+                    <p>Mr. Alex Lee
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1999-2001/Sunny_Chan.jpg" />
+                    <p>Mr. Sunny Chan
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="even-line chairman-holder">
+        	<div class="holder">
+            	<h3>1997 - 1999
+                <span>The 5th General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1997-1999/Aaron_Shum.jpg" />
+                    <p>Mr. Aaron Shum
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1997-1999/Dennis_Ng.jpg" />
+                    <p>Dr. Dennis Ng
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1997-1999/Nelson_Ho.jpg" />
+                    <p>Mr. Nelson Ho
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="odd-line chairman-holder">
+        	<div class="holder">
+            	<h3>1995 - 1997
+                <span>The 4th General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1995-1997/Karl_Shin.jpg" />
+                    <p>Mr. Karl Shin
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1995-1997/Henry_Chiu.jpg" />
+                    <p>Mr. Henry Chiu
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1995-1997/Lorenzo_Yih.jpg" />
+                    <p>Mr. Lorenzo Yih
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="even-line chairman-holder">
+        	<div class="holder">
+            	<h3>1993 - 1995
+                <span>The 3rd General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1993-1995/Eddie_Cheung.jpg" />
+                    <p>Mr. Eddie Cheung
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1993-1995/Sunny_Chan.jpg" />
+                    <p>Mr. Sunny Chan
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1993-1995/Peter_Siu.jpg" />
+                    <p>Mr. Peter Siu
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="odd-line chairman-holder">
+        	<div class="holder">
+            	<h3>1991 - 1993
+                <span>The 2nd General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1991-1993/Karl_Shin.jpg" />
+                    <p>Mr. Karl Shin
+                    <span>Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1991-1993/David_Diniz.jpg" />
+                    <p>Mr. David Diniz
+                    <span>Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1991-1993/David_Chui.jpg" />
+                    <p>Mr. David Chui
+                    <span>Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="even-line chairman-holder last">
+        	<div class="holder">
+            	<h3>1988 - 1991
+                <span>The 1st General Committee</span></h3>
+                
+                <div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1988-1991/Frank_Tang.jpg" />
+                    <p>Mr. Frank Tang
+                    <span>Founding Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1988-1991/Tse_Sui_Luen.jpg" />
+                    <p>Mr. Tse_Sui_Luen
+                    <span>Founding Associate Chairman</span>
+                    </p>
+                </div><!--
+                --><div class="chairman-item">
+                	<img src="../images/about-us/list-of-chairman/1988-1991/Karl_Shin.jpg" />
+                    <p>Mr. Karl Shin
+                    <span>Founding Associate Chairman</span>
+                    </p>
+                </div>
+            </div>
+        </div>
         
     </div>
 
